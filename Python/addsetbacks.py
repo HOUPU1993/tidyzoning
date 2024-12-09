221995bef1041c611111b9eadd7ddc3e80c78e89
HOUPU1993 <128195276+HOUPU1993@users.noreply.github.com>
2024-12-09 12:25:42 -0500

    test

--- Python/addsetbacks.py
+++ Python/addsetbacks.py
@@ -1,13 +1,2 @@
-import math
-
-def calculate_circle(radius):
-    if radius <= 0:
-        return "Radius must be greater than 0"
-    
-    area = math.pi * (radius ** 2)
-    circumference = 2 * math.pi * radius
-    
-    return {
-        "Area": round(area, 2),
-        "Circumference": round(circumference, 2)
-    }
+def hello_world():
+    print("Hello, World!")
