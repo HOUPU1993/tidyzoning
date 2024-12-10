de68a4d15c84b640987334e7ff99f2c6e2b25798
HOUPU1993 <128195276+HOUPU1993@users.noreply.github.com>
2024-12-10 16:12:27 -0500

    test

--- tidyzoning/find_district_idx.py
+++ tidyzoning/find_district_idx.py
@@ -35,5 +35,6 @@ def find_district_idx(tidyparcel, tidyzoning):
 
     # Replace NaN zoning_id with None for consistency
     results_df["zoning_id"] = results_df["zoning_id"].where(pd.notna(results_df["zoning_id"]), None)
+    results_df["zoning_id"] = results_df["zoning_id"].astype(str)
 
     return results_df
