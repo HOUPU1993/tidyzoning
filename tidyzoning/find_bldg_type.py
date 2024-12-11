82d8c307d6c4726c5543e4cb61fd61f5e0c80ea8
HOUPU1993 <128195276+HOUPU1993@users.noreply.github.com>
2024-12-11 16:53:39 -0500

    test

--- tidyzoning/find_bldg_type.py
+++ tidyzoning/find_bldg_type.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
 def find_bldg_type(tidybuilding):
+    """
+    Determines the building type based on the number of units in the tidybuilding DataFrame.
+
+    Parameters:
+        tidybuilding (pd.DataFrame): A DataFrame containing unit information for a building.
+                                     Columns include'units_0bed', 'units_1bed', 'units_2bed', 'units_3bed', and 'units_4bed'.
+                                     Each column represents the number of units of a specific type.
+
+    Returns:
+        str: A string representing the building type:
+             - "1_family", "2_family", or "3_family" for buildings with 1, 2, or 3 units.
+             - "4_family" for buildings with more than 3 units.
+             - "other" for buildings with no units or invalid data.
+    """
     # define the interested columns
     possible_columns = ["units_0bed", "units_1bed", "units_2bed", "units_3bed", "units_4bed"]
     
