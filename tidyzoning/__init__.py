7b602c594ef9816df9e8b97ce77de974181d0328
HOUPU1993 <128195276+HOUPU1993@users.noreply.github.com>
2025-01-28 18:29:27 -0500

    2222

--- tidyzoning/__init__.py
+++ tidyzoning/__init__.py
@@ -2,5 +2,4 @@ from .find_district_idx import find_district_idx
 from .find_bldg_type import find_bldg_type
 from .check_function import check_land_use
 from .get_zoning_req import get_zoning_req
-# from .check_footprint import check_footprint, fits, rect_fit, rot_fit
 from .check_footprint import check_footprint
